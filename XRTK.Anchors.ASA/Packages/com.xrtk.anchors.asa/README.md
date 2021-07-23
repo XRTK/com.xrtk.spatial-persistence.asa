0154d5b4ff226fb3b851eaf49766eb9c773b3ccb
Stephen Hodgson <hodgson.designs@gmail.com>
2021-07-23 06:54:24 -0700

    renamed project to XRTK.SpatialPersistence.ASA (#2)
    * renamed project to XRTK.SpatialPersistence.ASA
    
    * renamed a few lingering artifacts
    
    * fixed line endings
    
    * updated spatial persistence submodule

--- XRTK.Anchors.ASA/Packages/com.xrtk.anchors.asa/README.md
+++
@@ -1,3 +0,0 @@
-# Anchors.ASA
-
-The Anchors.ASA package for the [XRTK - Mixed Reality Toolkit](https://github.com/XRTK/XRTK-Core).
---
+++ XRTK.SpatialPersistence.ASA/Packages/com.xrtk.spatial-persistence.asa/Editor/SpatialPersistenceASAPathFinder.cs
@@ -0,0 +1,18 @@
+// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
+using XRTK.Editor.Utilities;
+
+namespace XRTK.SpatialPersistence.ASA.Editor
+{
+    /// <summary>
+    /// Dummy scriptable object used to find the relative path of the com.xrtk.spatial-persistence.asa.
+    /// </summary>
+    ///// <inheritdoc cref="IPathFinder" />
+    public class SpatialPersistenceASAPathFinder : ScriptableObject, IPathFinder
+    {
+        ///// <inheritdoc />
+        public string Location => $"/Editor/{nameof(SpatialPersistenceASAPathFinder)}.cs";
+    }
+}
