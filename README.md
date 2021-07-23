f4e3ee0f2898b0fb7470ba0c77364c077367106b
Stephen Hodgson <hodgson.designs@gmail.com>
2021-07-23 06:54:24 -0700

    renamed project to XRTK.SpatialPersistence.ASA (#2)
    * renamed project to XRTK.SpatialPersistence.ASA
    
    * renamed a few lingering artifacts
    
    * fixed line endings
    
    * updated spatial persistence submodule

---
+++ README.md
@@ -0,0 +1,3 @@
+# SpatialPersistence.ASA
+
+The SpatialPersistence.ASA package for the [XRTK - Mixed Reality Toolkit](https://github.com/XRTK/XRTK-Core).
---
+++ Runtime/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#define XRTK_SPATIAL_PERSISTENCE_ASA
+
+using System.Reflection;
+
+[assembly: AssemblyVersion("0.3.0")]
+[assembly: AssemblyTitle("com.xrtk.spatial-persistence.asa")]
+[assembly: AssemblyCompany("XRTK")]
+[assembly: AssemblyCopyright("Copyright (c) XRTK. All rights reserved.")]
